94bf147ab39f97a7d3544177ed9ab22164dd2b63
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-13 16:21:42 +0000

    Model e Migration db clientes - SoftDeletes

---
+++ database/migrations/2021_11_13_154244_create_clientes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Cliente;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', '100')->unique();
+            $table->string('telefone', '30')->unique();
+            $table->string('pais_residencia', '100')->nullable();
+            $table->string('cidade_residencia', '100')->nullable();
+            $table->string('estado_br', '100')->nullable();
+            $table->string('cidade_br', '100')->nullable();
+            $table->string('cpf', '11')->nullable();
+            $table->string('rg', '15')->nullable();
+            $table->string('passaporte', '15')->nullable();
+            $table->string('cnh', '15')->nullable();
+            $table->date('dt_nascimento')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
