4655683b2587769fed197812993ff6da16826601
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-30 09:55:24 +0000

    Implementando Login e JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,20 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+        // dd($request->all());
+        // autenticacao por email e senha -> metodo auth(verificar no config o tipo de autenticacao)
+        $credenciais = $request->all(['email', 'password']);
+        // retornar um JWT (Json Web Token)
+        $token = auth('api')->attempt($credenciais); // realiza uma tentativa de autenticacao e retorna token caso sucesso
+        // dd($token);
+        if($token){
+            return response()->json(['token' => $token], 200);
+        }else{
+            return response()->json(['erro' => 'Usuário ou senha inválido!'], 403);
+
+        }
+        
         return 'login';
     }
 
